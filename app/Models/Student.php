f94aeb8a5c3c4c290568509a870e752b5f031cc8
harsha <harshab6345@gmail.com>
2023-06-19 15:53:24 +0530

    I made all the necessary eloquent relations

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function subjects() {
+        return $this->hasMany(Subject::class);
+    }
+
+    public function attendance() {
+        return $this->hasOne(Attendance::class);
+    }
 }
--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Subject extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function Student() {
+        return $this->belongsTo(Student::class);
+    }
 }
